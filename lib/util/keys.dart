139ff91bc7abe3c33d4cd08cc14f1ff2720ec9c0
Michal <michal.cic@gmail.com>
2019-10-24 21:12:35 +0200

    update gitignore

--- lib/util/keys.dart
+++
@@ -1,6 +0,0 @@
-class Keys {
-  static final String _unsplashApiAccessKey =
-      'cd44f9ab51617016382888346fe3cdaa3c5a2d5f74371ab592f20a7249fe3b15';
-
-  static String get unsplashApiKey => _unsplashApiAccessKey;
-}
